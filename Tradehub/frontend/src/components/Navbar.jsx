ad5c7acf36298cc972ac94a353eb33962f0d31d0
prashant_rathour <prathour884@gmail.com>
2023-06-17 18:47:44 +0530

    add snacks_learn page

--- Tradehub/frontend/src/components/Navbar.jsx
+++ Tradehub/frontend/src/components/Navbar.jsx
@@ -1,104 +1,229 @@
-import React from 'react';
+import React from "react";
+import { NavLink, Link as RouterLink } from "react-router-dom";
+import tradehubImage from "../images/unnamed (1).jpg";
 import {
   Box,
   Flex,
-  Link,
+  Heading,
+  IconButton,
+  useDisclosure,
+  useBreakpointValue,
+  Drawer as ChakraDrawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  Stack,
   Button,
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
-  useBreakpointValue,
-} from '@chakra-ui/react';
-import { ArrowDownIcon } from '@chakra-ui/icons';
-import tradehubImage from '../images/unnamed (1).jpg';
+  Image,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 const Navbar = () => {
-  const menuItems = [
-    {
-      path: '/',
-      element: (
-        <Box fontSize={{ base: 'xl', md: '2xl' }} fontWeight="bold" display="flex" alignItems="center" borderRight={{ base: 'none', md: '1px solid gold' }} pr={4}>
-          <img src={tradehubImage} alt="" />
-          <Box ml={1} color="gold">
-            Tradehub
-          </Box>
-        </Box>
-      ),
-    },
-    {
-      path: '/',
-      element: (
-        <Menu>
-          <MenuButton
-            as={Button}
-            colorScheme="gold"
-            variant="link"
-            mr={4}
-            _hover={{ textDecoration: 'none' }}
-          >
-            <ArrowDownIcon />
-            What We Offer
-          </MenuButton>
-          <MenuList bg="black" color="gold">
-            <MenuItem as={Link} href="/" _hover={{ bg: 'gold' }}>
-              Invest
-            </MenuItem>
-            <MenuItem as={Link} href="/" _hover={{ bg: 'gold' }}>
-              Crypto
-            </MenuItem>
-            <MenuItem as={Link} href="/" _hover={{ bg: 'gold' }}>
-              Retirement
-            </MenuItem>
-            <MenuItem as={Link} href="/" _hover={{ bg: 'gold' }}>
-              Options
-            </MenuItem>
-            <MenuItem as={Link} href="/" _hover={{ bg: 'gold' }}>
-              Cash Card
-            </MenuItem>
-          </MenuList>
-        </Menu>
-      ),
-    },
-    { path: '/', element: 'Snacks' },
-    { path: '/', element: 'Learn' },
-    { path: '/', element: 'Support' },
-  ];
-
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const isMobile = useBreakpointValue({ base: true, md: false });
+  const isTablet = useBreakpointValue({ base: false, md: true });
 
   return (
-    <Flex bg="black" p={4} justifyContent="space-between" align="center" direction={isMobile ? 'column' : 'row'}>
+    <>
       <Box
-        color="gold"
-        display="flex"
-        alignItems="center"
-        flexWrap={isMobile ? 'wrap' : 'nowrap'}
+        bg="black"
+        color="white"
+        px={4}
+        py={2}
+        position="fixed"
+        top={0}
+        left={0}
+        right={0}
+        zIndex={1}
       >
-        {menuItems.map((menuItem, index) => (
-          <Box key={index} mr={4} mb={isMobile ? 2 : 0}>
-            {typeof menuItem.element === 'string' ? (
-              <Link
-                href={menuItem.path}
-                color="gold"
-                _hover={{ textDecoration: 'none' }}
-              >
-                {menuItem.element}
-              </Link>
-            ) : (
-              menuItem.element
-            )}
-          </Box>
-        ))}
+        <Flex
+          alignItems="center"
+          justifyContent={!isTablet ? "space-between" : "space-between"}
+        >
+          <NavLink to={"/"}>
+            <Flex alignItems="center" cursor={"pointer"}>
+              <Image
+                bg={"white"}
+                width={"50px"}
+                src={tradehubImage}
+                alt="logo"
+              />
+              <Heading color={"goldenrod"} size="md">
+                TRADEHUB
+              </Heading>
+            </Flex>
+          </NavLink>
+          {!isMobile && (
+            <>
+              <NavLinks />
+              <AuthButtons />
+            </>
+          )}
+          {isMobile && (
+            <IconButton
+              icon={<HamburgerIcon />}
+              aria-label="Open Menu"
+              variant="outline"
+              colorScheme="white"
+              onClick={onOpen}
+            />
+          )}
+        </Flex>
       </Box>
-      <Box>
-        <Button colorScheme="gold" variant="outline" mr={2}>
-          Login
-        </Button>
-        <Button colorScheme="gold">Sign up</Button>
-      </Box>
-    </Flex>
+      {isMobile && <MobileDrawer isOpen={isOpen} onClose={onClose} />}
+    </>
   );
 };
+
+const NavLinks = () => (
+  <Flex justifyContent={"space-between"} width={"30%"} m>
+    <Menu isLazy>
+      <MenuButton as={Button} colorScheme="white">
+        Stocks
+      </MenuButton>
+      <MenuList color={"gold"} fontWeight={"600"} bg={"black"}>
+        <MenuItem
+          as={RouterLink}
+          to="/invest"
+          border={"1px solid gray"}
+          p="5px"
+          borderRadius={"2xl"}
+          bg={"black"}
+        >
+          Invest
+        </MenuItem>
+        <MenuItem
+          as={RouterLink}
+          to="/stocks/yourportfolio"
+          border={"1px solid gray"}
+          p="5px"
+          borderRadius={"2xl"}
+          bg={"black"}
+        >
+          Your Portfolio
+        </MenuItem>
+        <MenuItem
+          as={RouterLink}
+          to="/stocks/allstocks"
+          border={"1px solid gray"}
+          p="5px"
+          borderRadius={"2xl"}
+          bg={"black"}
+        >
+          All Stocks
+        </MenuItem>
+      </MenuList>
+    </Menu>
+    <NavButton to="/snacks">Snacks</NavButton>
+    <NavButton to="/learn">Learn</NavButton>
+    <NavButton to="/support">Support</NavButton>
+  </Flex>
+);
+
+const NavButton = ({ to, children }) => (
+  <Button as={RouterLink} to={to} variant="ghost" colorScheme="white" ml={2}>
+    {children}
+  </Button>
+);
+
+const AuthButtons = () => (
+  <Box>
+    <Flex alignItems="center" boxSizing={"content-box"} margin={"auto"}>
+      <Button variant="ghost" colorScheme="white" mr={2}>
+        Login
+      </Button>
+      <Button colorScheme="white" mr={2}>
+        Sign Up
+      </Button>
+      <Button colorScheme="orange">Create Demat Account</Button>
+    </Flex>
+  </Box>
+);
+const AuthButtonsmob = () => (
+  <Box alignItems="center" p="5px" margin={"auto"}>
+    <Flex>
+      <Button variant="ghost" colorScheme="white" mr={2}>
+        Login
+      </Button>
+      <Button colorScheme="white" mr={2}>
+        Sign Up
+      </Button>
+    </Flex>
+    <Box>
+      <Button
+        overflow={"hidden"}
+        textOverflow={"ellipsis"}
+        whiteSpace={"nowrap"}
+        colorScheme="orange"
+      >
+        {" "}
+        Create Demat Account
+      </Button>
+    </Box>
+  </Box>
+);
+
+const MobileDrawer = ({ isOpen, onClose }) => (
+  <ChakraDrawer isOpen={isOpen} placement="left" onClose={onClose}>
+    <DrawerOverlay>
+      <DrawerContent bg="gray.800" color="white">
+        <DrawerCloseButton color="white" />
+        <DrawerHeader>Menu</DrawerHeader>
+        <DrawerBody>
+          <Stack spacing={4}>
+          
+            <Menu isLazy>
+              <MenuButton as={Button} colorScheme="white">
+                Stocks
+              </MenuButton>
+              <MenuList color={"gold"} fontWeight={"600"} bg={"black"}>
+                <MenuItem
+                  as={RouterLink}
+                  to="/invest"
+                  border={"1px solid gray"}
+                  p="5px"
+                  borderRadius={"2xl"}
+                  bg={"black"}
+                >
+                  Invest
+                </MenuItem>
+                <MenuItem
+                  as={RouterLink}
+                  to="/stocks/yourportfolio"
+                  border={"1px solid gray"}
+                  p="5px"
+                  borderRadius={"2xl"}
+                  bg={"black"}
+                >
+                  Your Portfolio
+                </MenuItem>
+                <MenuItem
+                  as={RouterLink}
+                  to="/stocks/allstocks"
+                  border={"1px solid gray"}
+                  p="5px"
+                  borderRadius={"2xl"}
+                  bg={"black"}
+                >
+                  All Stocks
+                </MenuItem>
+              </MenuList>
+            </Menu>
+            <NavButton to="/snacks">Snacks</NavButton>
+            <NavButton to="/learn">Learn</NavButton>
+            <NavButton to="/support">Support</NavButton>
+            <AuthButtonsmob />
+          </Stack>
+        </DrawerBody>
+      </DrawerContent>
+    </DrawerOverlay>
+  </ChakraDrawer>
+);
 
 export default Navbar;
